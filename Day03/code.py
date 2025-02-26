4ae0bc4024e00ff06efc740e7281ed4bc0174f9c
bilal-waleed <bilalwaleed246@gmail.com>
2025-02-27 01:26:53 +0500

    Day03

---
+++ Day03/code.py
@@ -0,0 +1,6 @@
+# Ask the user for their name and Display a high-tech welcome message
+
+name = input("Enter your name: ")
+
+print("\n🔹 Processing... Name detected: {} 🔍".format(name))
+print("🔹 Access Granted! Welcome, Agent {}. Your mission awaits! 🚀".format(name))
